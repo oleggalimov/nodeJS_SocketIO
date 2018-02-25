b765dee8e16c1edfd00255e4038fd616307c4b4b
oleggalimov <o.d.galimov@yandex.ru>
2018-02-25 23:37:48 +0500

    fix port env

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname,'../public');
 const socketIO = require('socket.io');
 const http = require('http');
