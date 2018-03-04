a6d59624ed2a53c2d04b3870c311d3016b7fa585
oleggalimov <o.d.galimov@yandex.ru>
2018-03-04 23:52:13 +0500

    loggin on

--- server/server.js
+++ server/server.js
@@ -5,16 +5,19 @@ const publicPath = path.join(__dirname,'../public');
 const socketIO = require('socket.io');
 const http = require('http');
 const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
+
+
 
 let app = express();
 let server = http.createServer(app);
-
+let users = new Users ();
 
 let IO = socketIO(server);
 app.use(express.static(publicPath));
 
 server.listen(port, ()=>{console.log(`Started on ${port}`)});
-let users = new Map();
+// let users = new Map();
 IO.on('connection', (socket)=>{
     // console.log('new User connected');
     // IO.emit('ServiceMessage', "New user has join!");//broadcast to all
@@ -22,27 +25,50 @@ IO.on('connection', (socket)=>{
     // socket.broadcast.emit('ServiceMessage', "New user has join!");
     socket.on ('join', (params, callback)=>{
       if (!isRealString(params.name)||!isRealString(params.room)) {
-        callback('Name and room are required!');
+        return callback('Name and room are required!');
       };
-      callback();  
-    } );
-    socket.on('newUser', (user)=>{
-        //IO.emit('ServiceMessage', `User "${user}" has join!`);//broadcast to all
+    //   socket.to('Room name').emit(); //сообщить всем в комнате
+    //   socket.broadcast.to().emit(); //сообщить всем кроме источника 
+      
+    
+      socket.join(params.room);
+      users.removeUser(socket.id);
+      users.addUser(socket.id, params.name, params.room);
+      IO.to(params.room).emit('updateUserList', users.getUserList(params.room));
+      socket.emit('ServiceMessage', `Welcome, ${params.name}!`);//сообщить только источнику
+      //IO.emit('ServiceMessage', `User "${user}" has join!`);//broadcast to all
         //save user in map
-        users.set(socket.id,user);
+        // users.set(socket.id,user);
         //console.log(users);
-        socket.broadcast.emit('ServiceMessage', `User "${user}" has join!`);//broadcast to all except current user
-    });
+        socket.broadcast.to(params.room).emit('ServiceMessage', `User "${params.name}" has join!`);//broadcast to all except current user
+      callback();
+
+      //socket.leave();  
+    } );
+    // socket.on('newUser', (user)=>{
+    //     //IO.emit('ServiceMessage', `User "${user}" has join!`);//broadcast to all
+    //     //save user in map
+    //     users.set(socket.id,user);
+    //     //console.log(users);
+    //     socket.broadcast.emit('ServiceMessage', `User "${user}" has join!`);//broadcast to all except current user
+    // });
     socket.on('sendMessage', (message,callback)=>{
+        let user = users.getUser(socket.id);
+        if (user && isRealString(message.text)) {
+            IO.to(user.roomName).emit('message',user.userName,message.text,message.date);
+        }
         console.log(message);
-        IO.emit('message',message);
+        
         callback();
     });
     socket.on('disconnect',()=>{
         // console.log('user is disconnected')
-        let userName = users.get(socket.id);
-        users.delete(socket.id);
-        socket.broadcast.emit('ServiceMessage', `User ${userName} leave the chat`);
+        
+        let user = users.removeUser(socket.id);
+        if (user) {
+            IO.to(user.roomName).emit('updateUserList',users.getUserList(user.roomName) );
+            IO.to(user.roomName).emit('ServiceMessage', `User ${user.userName} leave the chat`);
+        }
     });
 });
 
